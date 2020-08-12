f71df629cdde06e29dfd6be60c74fd83f95a19c8
Przemas91M <przemyslawiec@wp.eu>
2020-08-12 23:36:08 +0200

    Robię postępy w kursie

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,2 +1,13 @@
 public class Hello {
+    public static void main(String[] args){
+    System.out.println("Hello Przemek");
+
+    int mojPierwszyNumer = (10 + 5) + (2 * 10);
+    int mojDrugiNumer = 12;
+    int mojTrzeciNumer = mojPierwszyNumer * 2;
+    int suma = mojPierwszyNumer + mojDrugiNumer + mojTrzeciNumer;
+        System.out.println(suma);
+        int ostatni = 1000 - suma;
+        System.out.println(ostatni);
+    }
 }
